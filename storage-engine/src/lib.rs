49f53193b7e9868f9561d37183f2433af27fc1e5
kaoozhi <ztraphael@gmail.com>
2026-05-20 11:13:50 +0200

    feat: update lint level

--- storage-engine/src/lib.rs
+++ storage-engine/src/lib.rs
@@ -1,3 +1,28 @@
+#![deny(
+    unsafe_code,
+    missing_docs,
+    bad_style,
+    dead_code,
+    improper_ctypes,
+    non_shorthand_field_patterns,
+    no_mangle_generic_items,
+    overflowing_literals,
+    path_statements,
+    patterns_in_fns_without_body,
+    unconditional_recursion,
+    unused_allocation,
+    unused_comparisons,
+    unused_parens,
+    while_true,
+    missing_debug_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications
+)]
+
 pub mod chunk;
 pub mod compaction;
 pub mod config;
